b9936381d09fb8540bcd79737f4863efd2d10514
NotFunNy125 <funny.nott@gmail.com>
2024-05-25 11:52:21 +0200

    --

--- Source/CombatExtendedHardcore/obj/Debug/CombatExtendedHardcore.AssemblyInfo.cs
+++ Source/CombatExtendedHardcore/obj/Debug/CombatExtendedHardcore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("CC BY-NC-SA 4.0 2020")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.2.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.2+e47c57d2ee8b3889089eda5f547c52a79ec27414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.2+1907305bfd2ac8fce3e25716fdd214029f698cb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CombatExtendedHardcore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CombatExtendedHardcore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.2.0")]
