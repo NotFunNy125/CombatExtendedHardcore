fff7f6822226e4db29a7794b1a1ca7c4f5133ab8
NotFunNy125 <funny.nott@gmail.com>
2024-05-26 13:00:11 +0200

    first iteration

---
+++ Source/CombatExtendedHardcore/Harmony_StockGenerator_MarketValue_SelectionWeight.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CombatExtendedHardcore
+{
+    [HarmonyPatch(typeof(StockGenerator_MarketValue), "SelectionWeight")]
+    public static class Harmony_StockGenerator_MarketValue_SelectionWeight
+    {
+        public static bool Prefix(ref float __result, ThingDef thingDef)
+        {
+            if(thingDef.IsWeapon && thingDef.equipmentType == EquipmentType.Primary && !thingDef.weaponTags.NullOrEmpty<string>())
+            {
+                var countClass = thingDef.GetModExtension<WeaponCategories>();
+                if(countClass != null)
+                {
+                    var selectionWeight = countClass.categories[0].mainCategory.baseChance;
+                    __result = selectionWeight;
+                    return false;
+                }
+            } 
+            return true;
+        }
+    }
+}
